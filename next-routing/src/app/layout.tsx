96f1252139d48d611582ece5122754ff30deacfd
Sujon Ahmed <sujonahmed5284@gmail.com>
2026-02-23 23:10:55 +0600

    Handling Global Errors

--- next-routing/src/app/layout.tsx
+++ next-routing/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./inc/header";
+import { ErrorWrapper } from "./error-wrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,7 +34,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
-        {children}
+        <ErrorWrapper>{children}</ErrorWrapper>
       </body>
       <footer
         style={{
