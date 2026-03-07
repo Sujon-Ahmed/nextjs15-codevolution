070b1e9630644b4a523b3a63b6632c0d19036106
Sujon Ahmed <sujonahmed5284@gmail.com>
2026-03-07 21:58:49 +0600

    Handling PATCH Requests

--- next-route-handlers/app/comments/[id]/route.ts
+++ next-route-handlers/app/comments/[id]/route.ts
@@ -9,3 +9,15 @@ export async function GET(_request: Request, {params}: {params: Promise<{id: str
         return new Response(JSON.stringify({error: "Comment not found"}), { status: 404 });
     }
 }
+
+export async function PATCH(request: Request, {params} : {params: Promise<{id: string}>}) {
+    const {id} = await params;
+    const body = await request.json();
+    const comment = comments.findIndex(c => c.id === parseInt(id));
+    if(comment !== -1) {
+        comments[comment].text = body.text;
+        return new Response(JSON.stringify(comments[comment]));
+    } else {
+        return new Response(JSON.stringify({error: "Comment not found"}), { status: 404 });
+    }
+}
