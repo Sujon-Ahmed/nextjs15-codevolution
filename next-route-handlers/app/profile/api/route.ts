35ccfc6d55dbecb25e5e655596d1be5d4fcde457
Sujon Ahmed <sujonahmed5284@gmail.com>
2026-03-11 22:20:31 +0600

    Headers in Route Handlers

--- next-route-handlers/app/profile/api/route.ts
+++ next-route-handlers/app/profile/api/route.ts
@@ -1,3 +1,16 @@
-export async function GET() {
-    return new Response("Profile API Route");
+
+import { headers } from "next/headers";
+import { type NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+    // const requestHeaders = new Headers(request.headers);
+    // console.log(requestHeaders.get("Authorization"));
+
+    const headerList = await headers();
+    console.log(headerList.get("authorization"));
+    return new Response("<h1>Profile API Route</h1>", {
+        headers: {
+            "Content-Type": "text/html",
+        }
+    });
 }
