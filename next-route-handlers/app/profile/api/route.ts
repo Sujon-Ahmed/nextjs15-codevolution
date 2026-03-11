4bd15879363225a3bb7f2aa786fdc80a3a0af230
Sujon Ahmed <sujonahmed5284@gmail.com>
2026-03-11 22:41:47 +0600

    Cookies in Route Handlers

--- next-route-handlers/app/profile/api/route.ts
+++ next-route-handlers/app/profile/api/route.ts
@@ -1,5 +1,5 @@
 
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 import { type NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
@@ -8,9 +8,19 @@ export async function GET(request: NextRequest) {
 
     const headerList = await headers();
     console.log(headerList.get("authorization"));
+
+    // const theme = request.cookies.get("theme");
+    // console.log(theme);
+    
+    const cookieStore = await cookies();
+    cookieStore.set("resultPerPage", "20");
+    console.log(cookieStore.get("resultPerPage"));
+
+    
     return new Response("<h1>Profile API Route</h1>", {
         headers: {
             "Content-Type": "text/html",
+            "Set-Cookie": "theme=dark"
         }
     });
 }
